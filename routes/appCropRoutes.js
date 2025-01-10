d89a6808e62cc33fe289bdf73682dd28cf25b205
Harcharan Singh <bcs_2021028@iiitm.ac.in>
2025-01-09 23:33:58 -0800

    fix1

--- routes/appCropRoutes.js
+++ routes/appCropRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const cropController = require("../controllers/appCropAdvisory");
 
 // Route to get all crop names
-router.get("/crops", cropController.getAllCropNames);
+router.get("/all/crops", cropController.getAllCropNames);
 
 // Route to get detailed information based on crop name
 router.get("/crops/:name", cropController.getCropDetailsByName);
